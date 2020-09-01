32d41777db4af82f1d458fcf82ecccf20f4c6909
Julian Alsemmani <juliankonge@hotmail.com>
2020-09-01 17:31:23 +0200

    Changed the blocks and added a "algorithm".

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
-leftOrRight = ["BBRobotDirection.LEFT", "BBRobotDirection.RIGHT"]
+randomNumber = randint(0, 1)
 bitbot.led_rainbow()
-basic.show_string("Hei")
+basic.show_string("HEI")
 music.start_melody(music.built_in_melody(Melodies.BA_DING), MelodyOptions.ONCE)
 bitbot.go(BBDirection.FORWARD, 50)
 
 def on_forever():
+    global randomNumber
+    randomNumber = randint(0, 1)
+    bitbot.bb_bias(BBRobotDirection.LEFT, 10)
     basic.show_icon(IconNames.HAPPY)
-    if bitbot.sonar(BBPingUnit.CENTIMETERS) < 15:
-        bitbot.rotatems(BBRobotDirection.LEFT, 15, 2200)
-        if bitbot.sonar(BBPingUnit.CENTIMETERS) < 5:
-            bitbot.rotatems(BBRobotDirection.LEFT, 15, 2200)
+    if bitbot.sonar(BBPingUnit.CENTIMETERS) < 5:
+        bitbot.goms(BBDirection.REVERSE, 50, 500)
+    elif bitbot.sonar(BBPingUnit.CENTIMETERS) < 13:
+        bitbot.stop(BBStopMode.BRAKE)
+        if randomNumber == 0:
+            bitbot.rotatems(BBRobotDirection.LEFT, 22, 1200)
+        elif randomNumber == 1:
+            bitbot.rotatems(BBRobotDirection.RIGHT, 22, 1200)
     else:
         bitbot.go(BBDirection.FORWARD, 50)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,15 +1,22 @@
-let leftOrRight = ["BBRobotDirection.LEFT", "BBRobotDirection.RIGHT"]
+let randomNumber = randint(0, 1)
 bitbot.ledRainbow()
-basic.showString("Hei")
+basic.showString("HEI")
 music.startMelody(music.builtInMelody(Melodies.BaDing), MelodyOptions.Once)
 bitbot.go(BBDirection.Forward, 50)
 basic.forever(function () {
+    randomNumber = randint(0, 1)
+    bitbot.BBBias(BBRobotDirection.Left, 10)
     basic.showIcon(IconNames.Happy)
-    if (bitbot.sonar(BBPingUnit.Centimeters) < 15) {
-        bitbot.rotatems(BBRobotDirection.Left, 15, 2200)
-    } else {
-        if (bitbot.sonar(BBPingUnit.Centimeters) < 5) {
-            bitbot.rotatems(BBRobotDirection.Right, 15, 4400)
+    if (bitbot.sonar(BBPingUnit.Centimeters) < 5) {
+        bitbot.goms(BBDirection.Reverse, 50, 500)
+    } else if (bitbot.sonar(BBPingUnit.Centimeters) < 13) {
+        bitbot.stop(BBStopMode.Brake)
+        if (randomNumber == 0) {
+            bitbot.rotatems(BBRobotDirection.Left, 22, 1200)
+        } else if (randomNumber == 1) {
+            bitbot.rotatems(BBRobotDirection.Right, 22, 1200)
         }
+    } else {
+        bitbot.go(BBDirection.Forward, 50)
     }
 })
